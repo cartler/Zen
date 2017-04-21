914b694e408c3a899c9dcaf13bc9d60a5fa89be6
cartler <0073461@gmail.com>
2017-04-21 09:13:49 +0800

    add main func

--- zenCSV.py
+++ zenCSV.py
@@ -4,6 +4,27 @@
 import numpy
 import pandas
 
+pd = None
+
+def loadFile(fileName):
+    global pd
+    pd = pandas.read_csv(fileName)
+
+def prepareUrls(baseUrl):
+    urls = []
+    global pd
+
+    print(list(pd.columns))
+
+    for index in range(len(pd)):
+        # print(pd.loc[0,'com'])
+        params = list(pd.columns)
+        aa = baseUrl.format(com='111',id='222')
+        print(aa)
+
+    return urls
+
+
 if __name__ == '__main__':
     pd = pandas.read_csv('example_data.csv')
     # pdi = pd.reindex
--- zenMain.py
+++ zenMain.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
 #-*- coding: UTF-8 -*-
 
+import zenConfig
+import zenCSV
+
 def main():
-    pass
+    testDataFile = zenConfig.config['INPUT']['InputFile']
+    print('测试数据: ', testDataFile)
+
+    baseUrl = zenConfig.config['INPUT']['BaseURL']
+    print('baseUrl: ', baseUrl)
+
+    zenCSV.loadFile(testDataFile)
+    print(zenCSV.pd)
+
+    urls = zenCSV.prepareUrls(baseUrl)
+    print(urls)
+
+
 
 if __name__ == '__main__':
     main()
